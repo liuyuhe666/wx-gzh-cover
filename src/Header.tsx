4aac022a028a698f0d6ac1ee9b843f6df2c4ce87
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2025-01-16 18:47:27 +0800

    style: update

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <Flex align="center" justify="center">
-        <p style={{fontWeight: 900, fontSize: '24px'}}>WX-GZH-COVER</p>
+        <p style={{ fontWeight: 900, fontSize: '24px' }}>WX-GZH-COVER</p>
       </Flex>
     </>
   )
